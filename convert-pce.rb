55abba11fa6dbce548e2a8eb1ea7df6cefe7cb12
Brian <brian@zickzickzick.com>
2013-05-17 00:39:40 -0400

    Convert new text to BibleQE text format

---
+++ convert-pce.rb
@@ -0,0 +1,10 @@
+# open the file
+
+pce = File.new("texts/kjv-pce.txt")
+lines = pce.readlines
+lines.each do |line|
+	line.sub!(/[a-zA-Z0-9]* [0-9]*:[0-9]*/, '\0 ' + "\1")
+end
+File.open("texts/pce2.txt", "w") do |f|
+	lines.each {|l| f.puts l}
+end
