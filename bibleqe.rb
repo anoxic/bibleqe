0038a8088b8507d6e7f2c1c56e12e449ae0cb6b0
Brian <brian@zickzickzick.com>
2013-05-22 09:29:36 -0400

    IndexBuilder now writes a TOC file for the index

--- bibleqe.rb
+++ bibleqe.rb
@@ -54,7 +54,6 @@ class Index
 end
 
 class IndexBuilder
-	attr_reader :arr, :compiled
 	def initialize(name, dir = :texts)
 		@name = name
 		@dir = dir
@@ -66,7 +65,8 @@ class IndexBuilder
 		@indexversion = 1
 		@index = self.index
 		@compiled = self.compile
-		@arr = @index
+		@range = self.range
+		@compiled_range = self.compile_range
 	end
 	
 	def put
@@ -75,6 +75,7 @@ class IndexBuilder
 	
 	def write
 		File.open("#{@dir}/#{@name}.ind", "w") { |f| f << @compiled }
+		File.open("#{@dir}/#{@name}_toc.ind", "w") { |f| f << @compiled_range }
 	end
 	
 	def range(l = nil)
@@ -108,12 +109,19 @@ class IndexBuilder
 
 	def compile
 		out = "! BibleQE Index: #{@longversion}\n! version #{@indexversion}"
-		
 		@index = @index.sort
 		@index.each { |word, occ|
 			out << "\n#{word}#{occ}"
 		}
-		return out
+		out
+	end
+	
+	def compile_range
+		out = "! BibleQE Index TOC: #{@longversion}\n! version #{@indexversion}"
+		@range.each { |letter, range|
+			out << "\n#{letter} #{range[0]}..#{range[1]}"
+		}
+		out
 	end
 end
 
@@ -155,7 +163,7 @@ class Result
 end
 
 if __FILE__ == $0
-	p IndexBuilder.new(:kjv).range(:a)
+	IndexBuilder.new(:kjv).write
 	# result = Result.new(:pce2, ARGV.join(" "))
 	# puts result.matches
 	# puts result.show
