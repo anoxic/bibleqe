66f8cdec1b8b49bcc947d02d27df051cc5e8ba60
anoxic <brian@zickzickzick.com>
2013-08-28 17:41:44 -0400

    Added very very basic pagination to prompt

--- lib/prompt.rb
+++ lib/prompt.rb
@@ -1,8 +1,21 @@
 class Prompt
-	def initialize(*args)
+	def initialize(args)     
+	      
+	    # Use arg 1 for page number
+	    
+	    if args[0] =~ /^[0-9]+$/
+	        page = args[0].to_i
+            args.delete_at(0)
+	    else
+	        page = 1;
+	    end
+	                      
+	    # Get results
+	                      
 		result = Result.new(:kjv, args.flatten)
 		puts result.matches
+		puts "Page #{page}"
 		puts ""
-		puts result.show_by_page(1)
+		puts result.show_by_page(page)
 	end
 end
