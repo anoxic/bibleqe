2680b01c9cc39e0c47896b0d3914e2cb6d5ecea5
anoxic <brian@zickzickzick.com>
2013-09-03 10:02:22 +0000

    Added haml template for result page

--- app.rb
+++ app.rb
@@ -11,6 +11,30 @@ end
 
 get '/search' do
   redirect '/' if params[:q].nil?
-  x = Shell.new(params[:q] + " :all", false)
-  "<title>#{params[:q]}</title>" + x.out.gsub(/[\n\[\]]/, "\n"=>"<br>", "["=>"<i>", "]"=>"</i>")
+
+  parser = Parse.new(params[:q])
+  args = parser.args
+  options = parser.options
+
+  text = options[:text] ? options[:text] : :kjv
+
+  # Get results          
+  search = Search.new(text)
+  result = search.query(args)
+
+  out = []
+
+  # List results
+  if options[:list] != nil
+    out << ""
+    out << result.list.join(", ")
+  end
+
+  # Show results
+    out << ""
+    out << result.show!
+
+  #"<title>#{params[:q]}</title>" + x.out.gsub(/[\n\[\]]/, "\n"=>"<br>", "["=>"<i>", "]"=>"</i>")
+
+  haml :result, locals: {result: result}
 end
