8755b2ca3416dc90f6adabc41d6a9cad28485a00
anoxic <brian@zickzickzick.com>
2013-09-03 08:53:27 +0000

    Created Haml and Sass templates form home page

--- app.rb
+++ app.rb
@@ -1,23 +1,12 @@
 require 'sinatra'
+require 'haml'
 require_relative 'bibleqe'
 
 set :bind, '0.0.0.0'
 set :port, 80
 
 get '/' do
-  '<title>Bible Query Engine</title>
-  Search the Bible: <form action=/search><input name=q></form>
-  <pre>
-Arguments not starting in : are used as search terms (in an AND search)
-
-&lt;term&gt;|&lt;term&gt; executes an OR search
-
-/&lt;regex&gt;/ for a regular expression search
-
-:list list references
-
-:text &lt;version&gt; Change text  (by version abbreviation)
-  '
+  haml :index
 end
 
 get '/search' do
