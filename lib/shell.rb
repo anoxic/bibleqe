1cb4736cb9aa31cd345ec03b9f5323bce4178263
Brian <brian@zickzickzick.com>
2013-09-08 04:48:03 -0400

    Simplified the shell

--- lib/shell.rb
+++ lib/shell.rb
@@ -1,9 +1,7 @@
 class Shell
   attr_reader :out
 
-  def initialize(args, puts = true)
-    out = []
-
+  def initialize(args)
     parser = Parse.new(args)
     args = parser.args
     options = parser.options
@@ -23,31 +21,24 @@ class Shell
     result.limit = limit
 
     # Display line about matches
-    out << result.matches_verbose
+     puts result.matches_verbose
 
     # List results
     if options[:list] != nil
-      out << ""
-      out << result.list.join(", ")
-      return self.display out,puts
+      puts ""
+      puts result.list.join(", ")
+      return 
     end
 
     # Show results
     if options[:all] != nil
-      out << ""
-      out << result.show!
+      puts ""
+      puts result.show!
     else
-      out << "Page #{page} (displaying #{limit} results)" if result.matches > 0
-      out << ""
-      out << result.show_by_page!(page)
+      puts "Page #{page} (displaying #{limit} results)" if result.matches > 0
+      puts ""
+      puts result.show_by_page!(page)
     end
-
-    self.display out,puts
-  end
-
-  def display(out, puts)
-    puts out if puts == true
-    @out = out.join $/
   end
 end
 
