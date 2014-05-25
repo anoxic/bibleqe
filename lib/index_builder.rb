b037baee1da056b38c12a67376d9dec627ec711a
anoxic <brian@zickzickzick.com>
2014-05-25 23:18:28 +0000

    Replace tabs with spaces

--- lib/index_builder.rb
+++ lib/index_builder.rb
@@ -2,84 +2,84 @@ class IndexBuilder
   attr_reader :compiled
 
   def initialize(name, dir = :texts)
-  	@name = name
-  	@dir = dir
-  	
-  	t = Text.new(name, dir)
-  	@long_name = t.name
-  	@delim = t.delim
-  	@strip = t.strip
-  	@text = t.content
-  	
-  	@indexversion = 1
+    @name = name
+    @dir = dir
+    
+    t = Text.new(name, dir)
+    @long_name = t.name
+    @delim = t.delim
+    @strip = t.strip
+    @text = t.content
+    
+    @indexversion = 1
 
-  	@index = self.index
-  	@compiled = self.compile
+    @index = self.index
+    @compiled = self.compile
 
-  	@range = self.range
-  	@compiled_range = self.compile_range
+    @range = self.range
+    @compiled_range = self.compile_range
 
-  	@words = self.words
-  	@compiled_words = self.compile_words
+    @words = self.words
+    @compiled_words = self.compile_words
   end
   
   def put!
-  	print @compiled
+    print @compiled
   end
   
   def write
-  	File.open("#{@dir}/#{@name}.ind", "w") { |f| f << @compiled }
-  	File.open("#{@dir}/#{@name}_toc.ind", "w") { |f| f << @compiled_range }
-  	File.open("#{@dir}/#{@name}_words.lst", "w") { |f| f << @compiled_words }
+    File.open("#{@dir}/#{@name}.ind", "w") { |f| f << @compiled }
+    File.open("#{@dir}/#{@name}_toc.ind", "w") { |f| f << @compiled_range }
+    File.open("#{@dir}/#{@name}_words.lst", "w") { |f| f << @compiled_words }
   end
   
   def line(line, lineno, index)
     line.slice!(/^[a-zA-Z0-9]{1,4} [0-9]{1,3}:[0-9]{1,3} /)
-  	line.downcase!
-  	line.delete!(@strip)
+    line.downcase!
+    line.delete!(@strip)
 
-  	line.split.each do |k,v|
+    line.split.each do |k,v|
       index[k.to_sym] << " #{lineno},#{v.to_i + 1}"
     end
   end
 
   def index
-  	occurances = Hash.new{|h,k| h[k] = String.new}
-  	
-  	@text.each do |line|
-  		self.line(line, @text.lineno, occurances) unless line.start_with?('!','>','#') 
+    occurances = Hash.new{|h,k| h[k] = String.new}
+    
+    @text.each do |line|
+      self.line(line, @text.lineno, occurances) unless line.start_with?('!','>','#') 
     end
 
-  	occurances
+    occurances
   end
   
   def compile
-  	out = "! BibleQE Index: #{@long_name}\n! version #{@indexversion}"
-  	@index = @index.sort
-  	@index.each { |word, occ|
-  		out << "\n#{word}#{occ}"
-  	}
-  	out
+    out = "! BibleQE Index: #{@long_name}\n! version #{@indexversion}"
+    @index = @index.sort
+    @index.each { |word, occ|
+      out << "\n#{word}#{occ}"
+    }
+    out
   end
   
   def range(l = nil)
-  	words = @index.flatten.select {|x| x.is_a? Symbol }
-  	chars = Hash.new {|k,v| k[v] = Array.new(0) }
-  	words.each { |w|
-  		letter = w.to_s[0].to_sym
-  		chars[letter][0] ||= words.index(w) + 2
-  		chars[letter][1] = words.index(w) + 2
-  	}
-  	return chars[l.to_sym] unless l == nil
-  	chars
+    words = @index.flatten.select {|x| x.is_a? Symbol }
+    chars = Hash.new {|k,v| k[v] = Array.new(0) }
+    words.each { |w|
+      letter = w.to_s[0].to_sym
+      chars[letter][0] ||= words.index(w) + 2
+      chars[letter][1] = words.index(w) + 2
+    }
+    return chars[l.to_sym] unless l == nil
+    chars
   end
   
   def compile_range
-  	out = "! BibleQE Index TOC: #{@long_name}\n! version #{@indexversion}"
-  	@range.each { |letter, range|
-  		out << "\n#{letter} #{range[0]}..#{range[1]}"
-  	}
-  	out
+    out = "! BibleQE Index TOC: #{@long_name}\n! version #{@indexversion}"
+    @range.each { |letter, range|
+      out << "\n#{letter} #{range[0]}..#{range[1]}"
+    }
+    out
   end
 
   def words
