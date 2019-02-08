222fbb2551c09f73811964ffd55005716741433e
myklv <+@myklv.com>
2019-02-08 11:50:29 -0600

    move lib --> src

--- bibleqe.rb
+++ bibleqe.rb
@@ -1,13 +1,13 @@
-require_relative './lib/bibleqe.rb'
-require_relative './lib/text.rb'
-require_relative './lib/index.rb'
-require_relative './lib/index_builder.rb'
-require_relative './lib/search.rb'
-require_relative './lib/result.rb'
-require_relative './lib/shell.rb'
-require_relative './lib/parse.rb'
-require_relative './lib/verse.rb'
-require_relative './lib/string.rb'
+require_relative './src/bibleqe.rb'
+require_relative './src/text.rb'
+require_relative './src/index.rb'
+require_relative './src/index_builder.rb'
+require_relative './src/search.rb'
+require_relative './src/result.rb'
+require_relative './src/shell.rb'
+require_relative './src/parse.rb'
+require_relative './src/verse.rb'
+require_relative './src/string.rb'
 
 if __FILE__ == $0
   Shell.new(ARGV)
