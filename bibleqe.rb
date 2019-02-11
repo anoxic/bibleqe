ff2bec18be742243776ed4ffa2d9c63ce10d5b42
myklv <+@myklv.com>
2019-02-10 22:00:51 -0600

    move src/ back to lib/ per ruby convention

--- bibleqe.rb
+++ bibleqe.rb
@@ -1,11 +1,11 @@
-require_relative './src/bibleqe.rb'
-require_relative './src/text.rb'
-require_relative './src/index.rb'
-require_relative './src/indexer.rb'
-require_relative './src/search.rb'
-require_relative './src/result.rb'
-require_relative './src/shell.rb'
-require_relative './src/parse.rb'
+require_relative './lib/bibleqe.rb'
+require_relative './lib/text.rb'
+require_relative './lib/index.rb'
+require_relative './lib/indexer.rb'
+require_relative './lib/search.rb'
+require_relative './lib/result.rb'
+require_relative './lib/shell.rb'
+require_relative './lib/parse.rb'
 
 if __FILE__ == $0
   Shell.new(ARGV)
